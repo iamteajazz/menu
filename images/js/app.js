9c32aa01c063d93687ae12c2a74726bc3a302f27
Itive Elvis Ovie <117022841+iamteajazz@users.noreply.github.com>
2023-01-21 01:06:40 +0100

    Add files via upload

---
+++ images/js/app.js
@@ -0,0 +1,8 @@
+let myall = document.getElementById ("all");
+let godZilla = document.getElementById ("demo");
+
+myall.addEventListener ("click", myall);
+
+function myall (){
+    document.getElementById ("demo").innerHTML = "Hello World";
+}
