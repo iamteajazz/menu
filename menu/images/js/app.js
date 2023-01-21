1ddc2a0d043df9776f9b7d74be9ce2c9954ddfe2
Itive Elvis Ovie <117022841+iamteajazz@users.noreply.github.com>
2023-01-21 01:05:34 +0100

    Delete menu directory

--- menu/images/js/app.js
+++
@@ -1,8 +0,0 @@
-let myall = document.getElementById ("all");
-let godZilla = document.getElementById ("demo");
-
-myall.addEventListener ("click", myall);
-
-function myall (){
-    document.getElementById ("demo").innerHTML = "Hello World";
-}
